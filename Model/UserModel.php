52c9d1bc9370b4ad7b2b9084fab282e1571138b4
filipealmeida97 <filip.goal@gmail.com>
2024-01-16 17:39:52 -0300

    Identando a classe UserModel

--- Model/UserModel.php
+++ Model/UserModel.php
@@ -3,8 +3,10 @@ require_once ROOT_PATH ."/Model/Database.php";
 
 class UserModel extends Database{
 
-    public function getUsers(int $limit){
+    public function getUsers(int $limit) : array{
+
         return $this->select($limit);
+        
     }
 
 }
